34754dce8a58cc15ef5a90e98850105676aca433
Maximiliano Cidron <maximilianocidron@gmail.com>
2022-03-18 15:05:46 -0300

    Admin ident

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,63 +1,74 @@
 import React, { useState } from "react";
 //import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import AdminHeader from "./AdminHeader";
-import { Heading, Button, Text, Container, Box, Stat,
-  StatLabel,  StatNumber,  StatHelpText,  StatArrow,  StatGroup, Divider, Link } from '@chakra-ui/react'
+import {
+  Heading,
+  Button,
+  Text,
+  Container,
+  Box,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  Divider,
+  Link,
+} from "@chakra-ui/react";
 
 export default function Admin() {
+  let gamesList = useSelector((state) => state.games);
 
-  let gamesList = useSelector((state) => state.games)
+  const [game, setGame] = useState("");
+  const [user, setUser] = useState("");
 
-    const [game, setGame] = useState('')
-    const [user, setUser] = useState('')
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/?game=" + game).then((response) => {
+      setGame(response.data);
+    });
 
-  useEffect(() =>{
-    axios.get('http://localhost:3000/api/?game=' + game)
-    .then((response) =>{
-      setGame(response.data)
-    })
+    axios.get("http://localhost:3000/api/?user=" + user).then((response) => {
+      setUser(response.data);
+    });
 
-    axios.get('http://localhost:3000/api/?user=' + user)
-    .then((response) =>{
-        setUser(response.data)
-    })
-
-    return() => {
-        setGame(null)
-    }
-}, [game])
+    return () => {
+      setGame(null);
+    };
+  }, [game]);
 
   return (
-    <Container p='5'>
+    <Container p="5">
       <Box>
-      <AdminHeader />
+        <AdminHeader />
 
-      <Text fontSize='30px' mb='15px' mt='15px'>Nokler Stats</Text>
-      <Divider mb='15px'/>
-      <StatGroup>
-  <Stat>
-    <StatLabel>Games sold</StatLabel>
-    <StatNumber>345,670</StatNumber>
-    <StatHelpText>
-      <StatArrow type='increase' />
-      23.36%
-    </StatHelpText>
-  </Stat>
+        <Text fontSize="30px" mb="15px" mt="15px">
+          Nokler Stats
+        </Text>
+        <Divider mb="15px" />
+        <StatGroup>
+          <Stat>
+            <StatLabel>Games sold</StatLabel>
+            <StatNumber>345,670</StatNumber>
+            <StatHelpText>
+              <StatArrow type="increase" />
+              23.36%
+            </StatHelpText>
+          </Stat>
 
-  <Stat>
-    <StatLabel>Page views</StatLabel>
-    <StatNumber>45</StatNumber>
-    <StatHelpText>
-      <StatArrow type='decrease' />
-      9.05%
-    </StatHelpText>
-  </Stat>
-</StatGroup>
+          <Stat>
+            <StatLabel>Page views</StatLabel>
+            <StatNumber>45</StatNumber>
+            <StatHelpText>
+              <StatArrow type="decrease" />
+              9.05%
+            </StatHelpText>
+          </Stat>
+        </StatGroup>
       </Box>
-
     </Container>
   );
 }
