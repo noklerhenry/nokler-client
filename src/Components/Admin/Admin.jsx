95444ed3f1e4a23ddc1e56c93b1cf872a5b90cb3
Maximiliano Cidron <maximilianocidron@gmail.com>
2022-03-22 13:01:00 -0300

    Admin in progress

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -19,12 +19,22 @@ import {
   StatGroup,
   Divider,
   Link,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
 } from "@chakra-ui/react";
 
 export default function Admin() {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.orders);
   let gamesList = useSelector((state) => state.games);
+
+  const [scrollBehavior, setScrollBehavior] = useState("inside");
 
   const [game, setGame] = useState("");
   const [user, setUser] = useState("");
@@ -48,7 +58,7 @@ export default function Admin() {
   }, [dispatch]);
 
   const todayOrders = orders?.dateOrder?.reduce((result, order) => {
-    let day = moment(order.dateOrder).format("YYYY-MM-DD");
+    const day = moment(order.dateOrder.toISOString().split('T')[0]).format("YYYY-MM-DD");
     if (!result[day]) {
       result[day] = 0;
     }
@@ -56,8 +66,10 @@ export default function Admin() {
     return result;
   });
 
+  console.log(todayOrders)
+
   return (
-    <Container p="5">
+    <Container p="5" maxW={"8xl"}>
       <Box>
         <AdminHeader />
 
@@ -70,20 +82,68 @@ export default function Admin() {
             <StatLabel>Keys sold</StatLabel>
             <StatNumber>{orders?.length}</StatNumber>
             <StatHelpText>
-              <StatArrow/>
-              {todayOrders}
+              
             </StatHelpText>
           </Stat>
 
           <Stat>
-            <StatLabel>Page views</StatLabel>
-            <StatNumber>45</StatNumber>
+            <StatLabel>Keys sold today</StatLabel>
+            <StatNumber>{todayOrders}</StatNumber>
             <StatHelpText>
-              <StatArrow />
-              9.05%
+              
+            </StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatLabel>Total Income</StatLabel>
+            <StatNumber>$
+              {orders?.reduce(
+                (result, order) => result + order.price,
+                0
+              )}
+              </StatNumber>
+            <StatHelpText>
+              
             </StatHelpText>
           </Stat>
         </StatGroup>
+
+        <Table
+          variant="striped"
+          colorScheme="white"
+          value={scrollBehavior}
+          onChange={setScrollBehavior}
+        >
+          <TableCaption fontSize="xl">Keys sold</TableCaption>
+          <Thead>
+            <Tr>
+              <Th fontSize="xxl">Date</Th>
+              <Th fontSize="xxl">Sale ID</Th>
+              <Th fontSize="xxl">Mail</Th>
+              <Th fontSize="xxl">Game</Th>
+              <Th fontSize="xxl">Store</Th>
+              <Th fontSize="xxl">Key</Th>
+              <Th fontSize="xxl" isNumeric>
+                Price
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {orders?.map((order) => {
+              return (
+                <Tr>
+                  <Td>{order.dateOrder.slice(0, 10)}</Td>
+                  <Td>{order.id}</Td>
+                  <Td>{order.user.email}</Td>
+                  <Td>{order.game}</Td>
+                  <Td>{order.store}</Td>
+                  <Td>{order.key}</Td>
+                  <Td isNumeric>${order.price}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
       </Box>
     </Container>
   );
