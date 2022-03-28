029cc569f5c05684c7b62c5ee966ddec5229dbf0
brun0m4r <brunomarinich@gmail.com>
2022-03-28 15:22:32 -0300

    Update Gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -22,16 +22,14 @@ export default function Gallery() {
   return (
     <Container mt="200px" mb="150px">
       <Filter />
-      <Ordering/>
+      <Ordering />
       <SimpleGrid
         minChildWidth="250px"
         spacing="60px"
         backgroundImage=" linear-gradient(0deg, transparent 24%, rgba(140, 6, 247, .09) 25%, rgba(140, 6, 247, .09) 26%, transparent 27%, transparent 74%, rgba(140, 6, 247, .09) 75%, rgba(140, 6, 247, .05) 76%, transparent 77%, transparent), linear-gradient(90deg, transparent 24%, rgba(140, 6, 247, .09) 25%, rgba(140, 6, 247, .09) 26%, transparent 27%, transparent 74%, rgba(140, 6, 247, .09) 75%, rgba(140, 6, 247, .09) 76%, transparent 77%, transparent)"
         backgroundSize="24px 24px"
       >
-          <SeeMorePaginated
-            gamesFiltered={gamesFiltered}
-          />
+        <SeeMorePaginated gamesFiltered={gamesFiltered} />
       </SimpleGrid>
     </Container>
   );
